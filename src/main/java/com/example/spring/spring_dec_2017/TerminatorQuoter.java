599630c778d673c8bfeba814e179e05d37150cac
LeonchikEsp <lav09.01.95@gmail.com>
2017-12-03 20:47:13 +0300

    Constructor, PostConstruct annotation

--- src/main/java/com/example/spring/spring_dec_2017/TerminatorQuoter.java
+++ src/main/java/com/example/spring/spring_dec_2017/TerminatorQuoter.java
@@ -2,11 +2,24 @@ package com.example.spring.spring_dec_2017;
 
 import com.example.spring.spring_dec_2017.MyAnnotation.InjectRandomInteger;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInteger(min = 2, max = 7)
     private int repeat;
 
     private String message;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("PostConstruct");
+        System.out.println("repeat = " + repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Constructor");
+        System.out.println("repeat = " + repeat);
+    }
 
     public void setMessage(String message) {
         this.message = message;
