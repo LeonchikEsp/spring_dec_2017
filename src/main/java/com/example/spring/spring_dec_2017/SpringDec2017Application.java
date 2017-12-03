4cf769ce8b0a2d282e03459581ccef772a96e3b5
LeonchikEsp <lav09.01.95@gmail.com>
2017-12-03 19:19:55 +0300

    init

---
+++ src/main/java/com/example/spring/spring_dec_2017/SpringDec2017Application.java
@@ -0,0 +1,12 @@
+package com.example.spring.spring_dec_2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDec2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDec2017Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/spring/spring_dec_2017/SpringDec2017ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spring.spring_dec_2017;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDec2017ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
